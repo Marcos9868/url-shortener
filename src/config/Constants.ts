5ebad94298ad7f4c1d27f4d51c119c38f706a57e
Marcos9868 <marcos_m_ferreira@outlook.com>
2022-01-20 16:49:14 -0300

    Add mongo connection

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,3 +1,4 @@
 export const config = {
-  API_URL: "http://localhost:3000"
+  API_URL: "http://localhost:3000",
+  MONGO_CONNECTION: "mongodb+srv://Marcos:<password>@urlshortener.uly6y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 }
