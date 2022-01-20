025c02c20e937ce23135774c215529ab11476927
Marcos9868 <marcos_m_ferreira@outlook.com>
2022-01-20 15:48:35 -0300

    Set '/shorten' POST route

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import { UrlController } from 'controller/UrlController'
 
+// Set config on API
 const api = express()
+const urlController = new UrlController()
 
+// Able JSON
 api.use(express.json())
 
-api.get('/status', (req, res) => {
+// Status of API
+api.get('/status', (req: Request, res: Response) => {
   res.send({ name: 'Marcos', API: 1.0, year: 2022 })
 })
 
+// Hash
+api.post('/shorten', urlController.shorten)
+
+// Server Port
 api.listen(3000, () => {
   // tslint:disable-next-line: indent
   console.log('Server running at port http://localhost:3000/')
